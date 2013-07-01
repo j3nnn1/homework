a80fddcbccd5cff87b63f182f4d54b9172023072
j3nnn1 <jcmm986@gmail.com>
2013-07-01 00:34:20 -0300

    data files

---
+++ tp_vi/csvtogeojson.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+use strict;
+use common::sense;
+
+#jcmm986 - 0.0.0 -
+
+#read centroides
+#obtener arreglo de provincias y capitales con su long y lat.
+#read csv
+#if province_csv = province_geojson or city_geojson 
+    #add features in geojson
+
+
+
+
