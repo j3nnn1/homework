3cf13884a3d8aa4c7b4b51e46aee813be300666f
j3nnn1 <jcmm986@gmail.com>
2014-06-03 01:15:12 -0300

    all ml week 1

---
+++ machinelearning/design_study_prediction.r
@@ -0,0 +1,20 @@
+
+# define rate error
+# split training, test and validation
+# select feature, and select evaluation error cross-validation
+# rule of thumbs 
+# -- Large Datasets 
+# 60% training
+# 20% test
+# 20% validation
+
+# -- medium Datasets
+# 60% training
+# 40% testing
+
+# -- small sample size
+# crossvalidation.
+# report 
+# randomly sample dataset training
+# backtesting in finance, split in time chunks
+# balance features.
